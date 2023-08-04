26b8f906ffa4f11bd2b2321d19d7add0b32fced9
Valeria <valetheangel95@gmail.com>
2023-08-03 22:32:31 -0600

    archivos

---
+++ js/codigo2.js
@@ -0,0 +1,16 @@
+
+//alert agotado Zapatillas Nike
+let zapatillasNike=document.getElementById("Nike");
+zapatillasNike.addEventListener("click", alert_agotado);
+
+function alert_agotado(){
+    alert("Este artículo no se encuentra disponile");
+}
+
+let btnAgotado=document.getElementById("btn-agotado");
+btnAgotado.addEventListener("click", btn_agotado);
+
+function btn_agotado(){
+    alert("Este artículo no se encuentra disponible");
+}
+
