7e6565a3d8359c9f8acb50dcced87d1fdca48bc9
Valeria <valetheangel95@gmail.com>
2023-08-12 22:41:01 -0600

    archivos

--- js/codigo5.js
+++ js/codigo5.js
@@ -1,105 +1,64 @@
-
-
-//Validacion Formulario contacto
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const expresiones={
-
-nombre:" [A-Za-z]{4-16}",
-correo:"([a-zA-Z0-9]([^ @&%$\\\/()=?¿!.,:;]?|\d?)+[a-zA-Z0-9][\.]){1,2} ",
-
-comentarios:"^.{1,255}$",
-
-
-}
-
-
-
-
-
-let form = document.getElementById("formulario");
-let inputs=document.querySelectorAll("#formulario input")
-
-const validarFormulario=()=>{
-   switch(e.target.name){
-    case"nombre":
-    if(expresiones.nombre.test(e.target.value)){
-
-    }else{
-       alert("Este campo es inválido")
-    }
-    break;
-    case "correo":
-        if(expresiones.correo.test(e.target.value)){
-
-        }else{
-           alert("Este campo es inválido")
-        }
-    break;
-    case "comentarios":
-        if(expresiones.comentarios.test(e.target.value)){
-
-        }else{
-           alert("Este campo es inválido")
-        }
-    break;
+$(document).ready(function () {
+   $("#img-carrito").click(function () {
+         $("#detalle-compra").slideToggle(300);
+   });
+
+   $("#enviando").click(function (e) {
+      e.preventDefault();
+      //validar inputs
+      
+      const $nombre = document.getElementById("nombre");
+      const $email = document.getElementById("email");
+      const $telefono = document.getElementById("telefono");
+      const $comentario = document.getElementById("comentario");
+
+      const regexNombre = new RegExp("^[a-zA-Z]{4,16}$");
+      const regexEmail = new RegExp("[^@\s]+@[^@\s]+\.[^@\s]+");
+      const regexTelefono = new RegExp("^[0-9]{8,12}$");
+      const regexComentario = new RegExp("^.{1,255}$");
+
+      let validacion;
+      validacion = validar($nombre, regexNombre);
+      if (!validacion) return false;
+      validacion = validar($email, regexEmail);
+      if (!validacion) return false;
+      validacion = validar($telefono, regexTelefono);
+      if (!validacion) return false;
+      validacion = validar($comentario, regexComentario);
+      if (!validacion) return false;
+
+      
+
+      //envio el formulario
+      // $("#div_mensaje_enviando").show();
+
+      // setTimeout(() => {
+      //    $("#div_mensaje_enviando").hide();
+      //    $("#form").submit();
+      // }, 5000);
+   });
+});
+
+const validar = (input, expreg) => { 
+
+   let resultado = expreg.test(input.value)  
+   console.log(input);
+   
+   if (!resultado) {
+      input.classList.remove('inputSuccess');
+      input.classList.add('inputError');
+      input.focus();
+      Swal.fire({
+         title: "Error",
+         text: `El campo ${input.id.toUpperCase()} no tiene un formato válido`,
+         icon: "error",
+         confirmButtonText: "Aceptar",
+         confirmButtonColor: "#FD5D9D",
+      })
+      return false;
+   } else {
+      input.classList.remove('inputError');
+      input.classList.add('inputSuccess');
+      return true;
    }
 }
-inputs.forEach((input)=>{
-input.addEventListener("keyup",validarFormulario);
-input.addEventListener("blur",validarFormulario);
-})
-
-//form.addEventListener("submit",(e)=>{
-   // e.preventDefault();
-
-
-//})*/
