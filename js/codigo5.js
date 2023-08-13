00ec217fa4cdb5386623161e5b16d56b9400d231
Valeria <valetheangel95@gmail.com>
2023-08-12 19:51:58 -0600

    archivos

--- js/codigo5.js
+++ js/codigo5.js
@@ -1,42 +1,105 @@
 
-//suscripcion 
-
-let form=document.querySelector("form")
-let input=document.querySelector("input")
-let eventoRemover= document.getElementById("btn-suscripcion")
-
-let remover= function(e){
-    eventoRemover.removeEventListener("click", remover);
-    eventoRemover.disabled=true;
-}
-
-
-
-
-form.addEventListener("submit", (e)=>{
-e.preventDefault()
-let valor=input.value
-let valor_trim=valor.trim()
-let longitud=valor_trim.length
-console.log(encodeURIComponent(valor_trim))
-
-if(longitud>10){
-alert("gracias por suscribirte")
-remover();
-}
-else{
-    alert("Aun hay campos sin completar y/o no validos")
-}
-
-})
-
 
 //Validacion Formulario contacto
 
-let d=document;
 
-  function formularioContacto() {
-    const $form= d.querySelector(".formulario")
-    let input= d.querySelectorAll(".formulario[required]");
-    console.log(input);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const expresiones={
+
+nombre:" [A-Za-z]{4-16}",
+correo:"([a-zA-Z0-9]([^ @&%$\\\/()=?¿!.,:;]?|\d?)+[a-zA-Z0-9][\.]){1,2} ",
+
+comentarios:"^.{1,255}$",
+
+
 }
+
+
+
+
+
+let form = document.getElementById("formulario");
+let inputs=document.querySelectorAll("#formulario input")
+
+const validarFormulario=()=>{
+   switch(e.target.name){
+    case"nombre":
+    if(expresiones.nombre.test(e.target.value)){
+
+    }else{
+       alert("Este campo es inválido")
+    }
+    break;
+    case "correo":
+        if(expresiones.correo.test(e.target.value)){
+
+        }else{
+           alert("Este campo es inválido")
+        }
+    break;
+    case "comentarios":
+        if(expresiones.comentarios.test(e.target.value)){
+
+        }else{
+           alert("Este campo es inválido")
+        }
+    break;
+   }
+}
+inputs.forEach((input)=>{
+input.addEventListener("keyup",validarFormulario);
+input.addEventListener("blur",validarFormulario);
+})
+
+//form.addEventListener("submit",(e)=>{
+   // e.preventDefault();
+
+
+//})*/
